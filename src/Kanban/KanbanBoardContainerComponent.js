a326359b47e8baaef82f61c9a755f45c13cfd159
ayoub <ayoub.belghar@gail.com>
2016-08-16 17:03:42 +0100

    add taskCallbacks traitement

--- src/Kanban/KanbanBoardContainerComponent.js
+++ src/Kanban/KanbanBoardContainerComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import { KanbanBoardComponent } from './KanbanBoardComponent.js';
 import 'whatwg-fetch';
+import 'babel-polyfill';
+import  update  from 'react-addons-update';
 
 const API_URL = 'http://kanbanapi.pro-react.com';
 const API_HEADERS = {
@@ -18,23 +20,87 @@ export class KanbanBoardContainerComponent extends Component {
   }
 
   addTask(cardId, taskName){
-    console.log("addTask");
-    console.log("cardId", cardId);
-    console.log("taskName", taskName);
+    // Find the index of the card
+    let cardIndex = this.state.cards.findIndex((card) => card.id === cardId);
+
+    // Create a new task with the given name and a temporary ID
+    let newTask = { id: Date.now(), name: taskName, done: false };
+
+    // Create a new object and push the new task to the array of tasks
+    let nextState = update(this.state.cards, {
+      [cardIndex]: {
+        tasks: { $push: [newTask] }
+      }
+    });
+
+    // Set the Component state to the mutated object
+    this.setState({ cards: nextState});
+
+    // Call the API to add the task on the server
+    fetch(`${API_URL}/cards/${cardId}/tasks`, {
+      method: 'post',
+      headers: API_HEADERS,
+      body: JSON.stringify(newTask)
+    })
+    .then((response) => response.json())
+    .then((responseData) => {
+      // When the server return the definitive ID
+      // used to the new Task on the servern, update it on React
+      newTask.id = responseData.id;
+      this.setState({ cards: nextState });
+    });
+
   }
 
   deleteTask(cardId, taskId, taskIndex){
-    console.log("deleteTask");
-    console.log("cardId", cardId);
-    console.log("taskId", taskId);
-    console.log("taskIndex", taskIndex);
+    let cardIndex = this.state.cards.findIndex((card) => card.id === cardId);
+
+    // Create new Obejct without the task
+    let nextState = update(this.state.cards, {
+      [cardIndex]: {
+        tasks: { $splice: [[taskIndex, 1]] }
+      }
+    });
+
+    // set the component state to the mutated object
+    this.setState({ cards: nextState });
+
+    // Call the API to remove the task on the server
+    fetch(`${API_URL}/cards/${cardId}/tasks/${taskId}`, {
+      method: 'delete',
+      headers: API_HEADERS
+    });
   }
 
   toggleTask(cardId, taskId, taskIndex){
-    console.log("toggleTask");
-    console.log("cardId", cardId);
-    console.log("taskId", taskId);
-    console.log("taskIndex", taskIndex);
+
+    let cardIndex = this.state.cards.findIndex((card) => card.id === cardId);
+    // Save a reference to the task's 'done' value
+    let newDoneValue ;
+    // Using $apply command, you will change the done value to its opposite
+    let nextState = update(this.state.cards, {
+      [cardIndex]: {
+        tasks: {
+          [taskIndex]: {
+            done: {
+              $apply: (done) => {
+                newDoneValue = !done;
+                return newDoneValue;
+              }
+            }
+          }
+        }
+      }
+    });
+    // Set the component state to mutate object
+    this.setState({ cards: nextState });
+
+    // Call the API to toggle the task on the server
+    fetch(`${API_URL}/cards/${cardId}/tasks/${taskId}`, {
+      method: 'put',
+      headers: API_HEADERS,
+      body: JSON.stringify({ done: newDoneValue })
+    });
   }
 
   componentDidMount(){
